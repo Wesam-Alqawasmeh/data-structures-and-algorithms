694cde44b1304d6bd7f37a6cc6813e927dde7304
Wesam-Alqawasmeh <alqawasmehwesam@gmail.com>
2021-10-29 16:43:00 +0200

    challenge 12 edit

--- javascript/code-challenges/challenges-12.test.js
+++ javascript/code-challenges/challenges-12.test.js
@@ -109,7 +109,9 @@ Return either true or false.
 
 const validatePhoneNumber = (phoneNumber) => {
   // Solution code here...
+let regex = /^[+]?(1\-|1\s|1|\d{3}\-|\d{3}\s|)?((\(\d{3}\))|\d{3})(\-|\s)?(\d{3})(\-|\s)?(\d{4})$/g ;
 
+return(regex.test(phoneNumber));
   
 };
 

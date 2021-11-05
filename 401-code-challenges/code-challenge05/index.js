6dfa28d9895dc423efe63c00eaada32bf10366c8
Wesam-Alqawasmeh <alqawasmehwesam@gmail.com>
2021-11-05 21:38:29 +0200

    challenge 05

---
+++ 401-code-challenges/code-challenge05/index.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const LinkedList = require('./lib/LinkedList');
+
+
+const newLinkedList = new LinkedList();
+
+
+newLinkedList.insert('a');
+newLinkedList.insert('b');
+newLinkedList.insert('c');
+newLinkedList.insert('e');
+newLinkedList.insert('f');
+newLinkedList.insert('g');
+
+
+
+console.log(newLinkedList);

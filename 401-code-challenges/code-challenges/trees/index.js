5e59e55e14571b95a2d22ce0517ce3a3eb0220d8
Wesam-Alqawasmeh <alqawasmehwesam@gmail.com>
2021-11-23 21:15:38 +0200

    401 CC18

--- 401-code-challenges/code-challenges/trees/index.js
+++ 401-code-challenges/code-challenges/trees/index.js
@@ -8,25 +8,55 @@ const BinaryTree = require("./BinaryTree");
 
 const breadthFirst = require("./breadthFirst");
 
+const fizzBuzz = require("./fizzbuzz");
+
 const Node = require("./Node");
 
 const newBinarySearchTree = new BinarySearchTree();
 
 const newBinaryTree = new BinaryTree();
 
-newBinaryTree.root = new Node(
-  2,
-  new Node(7, new Node(2), new Node(6, new Node(5), new Node(11))),
-  new Node(5, null, new Node(9, new Node(4)))
-);
+let k_aryTree = {
+  value: 1,
+  children: {
+    firstChild: {
+      value: 3,
+      children: {
+        firstChild: {
+          value: 30,
+        },
+        secondChild: {
+          value: 45,
+        },
+        thirdChild: {
+          value: 10,
+        },
+      },
+    },
+    secondChild: {
+      value: 5,
+    },
+    thirdChild: {
+      value: 15,
+    },
+  },
+};
 
-newBinarySearchTree.root = new Node(
-  10,
-  new Node(5, new Node(3), new Node(6)),
-  new Node(15, new Node(14), new Node(18))
-);
+console.log(fizzBuzz(k_aryTree));
 
-console.log(breadthFirst(newBinaryTree.root));
+// newBinaryTree.root = new Node(
+//   2,
+//   new Node(7, new Node(2), new Node(6, new Node(5), new Node(11))),
+//   new Node(5, null, new Node(9, new Node(4)))
+// );
+
+// newBinarySearchTree.root = new Node(
+//   10,
+//   new Node(5, new Node(3), new Node(6)),
+//   new Node(15, new Node(14), new Node(18))
+// );
+
+// console.log(breadthFirst(newBinaryTree.root));
 
 // newBinarySearchTree.add(19);
 // newBinarySearchTree.add(13);
